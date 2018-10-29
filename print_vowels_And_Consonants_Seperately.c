5eee064bb2bce1ee975b0a48ec28116451324708
CHIRAG <chiragchopra7@gmail.com>
2018-10-29 15:58:46 +0530

    Added a code for printing the vowels and consonants of a string seperately.

---
+++ print_vowels_And_Consonants_Seperately.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+ 
+int main()
+{
+    char text[100];
+    int i;
+     
+    printf("Enter any string: ");
+    gets(text);
+     
+    printf("String is: ");
+    for(i=0;text[i]!='\0';i++)
+    {
+        printf("%c",text[i]);
+    }
+    printf("\n");
+     
+    printf("VOWEL Characters are: ");
+    for(i=0;text[i]!='\0';i++)
+    {
+        if(text[i]=='A' || text[i]=='a' || text[i]=='E' || text[i]=='e' || text[i]=='I' || text[i]=='i' || text[i]=='O' || text[i]=='o' || text[i]=='U' || text[i]=='u')
+            printf("%c",text[i]);
+    }
+    printf("\n");
+     
+    printf("CONSONANT Characters are: ");
+    for(i=0;text[i]!='\0';i++)
+    {
+        if(!(text[i]=='A' || text[i]=='a' || text[i]=='E' || text[i]=='e' || text[i]=='I' || text[i]=='i' || text[i]=='O' || text[i]=='o' || text[i]=='U' || text[i]=='u'))
+            printf("%c",text[i]);
+    }
+    printf("\n");
+     
+    return 0;
+}
